7b5cc60af845e4775f2db4126dc62fa841b7f11c
Ammubujju <51942968+Ammubujju@users.noreply.github.com>
2019-08-07 22:42:04 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -61,11 +61,16 @@ public class TrackServiceImpl implements TrackService {
     }
 
 
-
-    @Override
-    public void deleteTrack(int id) {
-        trackRepository.deleteById(id);
+@Override
+    public Track deleteTrack(int id) {
+        Optional<Track> track =null;
+        if(trackRepository.existsById(id) == true) {
+            trackRepository.deleteById(id);
+            track= trackRepository.findById(id);
+        }
+        return track.get();
     }
+
     @Override
     public Track trackByName(String firstName)  {
         return trackRepository.trackByName(firstName);
